20b3a59117245f18aee98adc91c922ab08ee6c1a
camilo20000403 <63866558+camilo20000403@users.noreply.github.com>
2020-09-09 21:07:37 -0500

    Add files via upload

---
+++ Pulsacione/Pulsacione/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pulsacione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+
+            int edad;
+            String sexo;
+
+            int pulsaciones = 0;
+            Console.WriteLine("Digite el nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Digite el Sexo (f/m)");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Digite la Edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+           
+            if (sexo == "f")
+            {
+                pulsaciones = (220 - edad) / 10;
+
+            }
+            else if (sexo == "m")
+            {
+                pulsaciones = (210 - edad) / 10;
+
+            }
+
+
+            Console.WriteLine("las pulsaciones de " + nombre + "  son de  " + pulsaciones);
+            Console.ReadLine();
+            }
+        }
+    }
+
